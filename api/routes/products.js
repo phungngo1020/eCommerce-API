5bca7828f8d954f7fe34a89bf93e3b08cd501403
Phung Ngo <phungngo@Phungs-iMac.fios-router.home>
2020-04-26 20:19:18 -0400

    first commit

---
+++ api/routes/products.js
@@ -0,0 +1,173 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+        cb(null, true);    // accept a file
+    } else {
+        cb(null, false);    // reject a file
+    }
+};
+
+const upload = multer({
+    storage: storage, 
+    limits: {
+        fileSize: 1024 * 1024 * 5  
+    },
+    fileFilter: fileFilter
+});
+
+const Product = require('../models/product');
+
+router.get("/", (req, res, next) => {
+    Product.find()
+        .select("name price _id productImage")
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {    // return information about what you can do w the object
+                        name: doc.name,
+                        price: doc.price,
+                        productImage: doc.productImage,
+                        _id: doc._id,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:3002/products/" + doc._id
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
+    console.log(req.file);
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price,
+        productImage: req.file.path
+    });
+    product
+        .save()
+        .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "Created product successfully",
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3002/products/" + result._id
+                }
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+    
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+        .select('name price _id productImage')
+        .exec()
+        .then(doc => {
+            console.log("From database", doc);
+            if (doc) {
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3002/products'
+                    }
+                });
+            } else { // Id in right format but returns null (doesn't exist)
+                res.status(404).json({message: 'No valid entry for provided ID'});
+            }
+        })
+        .catch(err => { // Id in wrong format or invalid id
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+});
+
+// patching = changing but keep id, and set of properties
+//[ {"propName": "name", "value": "Airpods 2"} ]
+router.patch('/:productId', checkAuth, (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({_id: id}, { $set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3002/products/' + id
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.delete('/:productId',checkAuth,  (req, res, next) => {
+    const id = req.params.productId;
+    Product.remove({_id: id})
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'Product deleted',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:3002/products',
+                body: { name: 'String', price: 'Number' }
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+module.exports = router;
